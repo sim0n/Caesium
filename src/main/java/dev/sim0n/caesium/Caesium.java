ba7b5ca266c91d61cd028f3f2015eeef74cd30a5
Simon <simonvin@protonmail.com>
2020-11-18 13:58:04 +0100

    Update version

--- src/main/java/dev/sim0n/caesium/Caesium.java
+++ src/main/java/dev/sim0n/caesium/Caesium.java
@@ -16,7 +16,7 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 @Getter
 public class Caesium {
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
 
     private static final String SEPARATOR = Strings.repeat("-", 30);
 
