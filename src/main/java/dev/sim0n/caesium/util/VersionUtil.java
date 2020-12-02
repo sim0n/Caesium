2db63c2a28782b2e53cc17b0a57c354437be5456
GitRowin <rowin2407@gmail.com>
2020-12-02 17:24:11 +0100

    Use @UtilityClass.

--- src/main/java/dev/sim0n/caesium/util/VersionUtil.java
+++ src/main/java/dev/sim0n/caesium/util/VersionUtil.java
@@ -1,14 +1,16 @@
 package dev.sim0n.caesium.util;
 
 import dev.sim0n.caesium.Caesium;
+import lombok.experimental.UtilityClass;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+@UtilityClass
 public class VersionUtil {
 
-    public static String getVersion() {
+    public String getVersion() {
         try (InputStream is = Caesium.class.getResourceAsStream("/META-INF/maven/dev.sim0n/caesium/pom.properties")) {
             if (is != null) {
                 Properties properties = new Properties();
